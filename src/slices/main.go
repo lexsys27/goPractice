a729b854a742164e622c41fb857e2b3634370600
Aleksey Zalesov <aleksey.zalesov@altoros.com>
2016-04-04 12:34:36 +0300

    Dynamic slices: len vs cap

--- src/slices/main.go
+++ src/slices/main.go
@@ -13,11 +13,16 @@ func printer(words []string) {
 }
 
 func main() {
-  words := make([]string, 4)
-  words[0] = "the"
-  words[1] = "quick"
-  words[2] = "brown"
-  words[3] = "fox"
+  words := make([]string, 0, 4)
+  fmt.Printf("%d %d\n", len(words), cap(words))
+  words = append(words, "the")
+  words = append(words, "quick")
+  words = append(words, "brown")
+  words = append(words, "fox")
+  fmt.Printf("%d %d\n", len(words), cap(words))
+  words = append(words, "jumps")
+  fmt.Printf("%d %d\n", len(words), cap(words))
+
 
   printer(words)
 }
