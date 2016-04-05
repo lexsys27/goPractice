bc99bd5e29fd9657153fa40e767bc6cb3b88f34e
Aleksey Zalesov <aleksey.zalesov@altoros.com>
2016-04-05 12:14:17 +0300

    Iterating over the map and counting

--- src/myMap/main.go
+++ src/myMap/main.go
@@ -10,8 +10,16 @@ func main() {
 	dayMonths["Feb"] = 28
 	dayMonths["Mar"] = 30
 
-  for month, days := range dayMonths {
-    fmt.Printf("%s has %d days\n", month, days)
+  has31 := 0
+
+  delete(dayMonths, "Feb")
+
+  for _, days := range dayMonths {
+    if days == 31 {
+      has31++
+    }
   }
+
+  fmt.Printf("%d months have 31 days\n", has31)
 
 }
